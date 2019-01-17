132fc6d70193722dcbc15dd4bcb77665a6432fbc
J.Lee <joverlee521@gmail.com>
2019-01-17 09:53:03 -0800

    added initial level system script

---
+++ scripts/levelSystem.js
@@ -0,0 +1,11 @@
+function calcNewLevelAndXP( currentLevel, currentXP, gainedXP) {
+    let leftOverXP = 0;
+    do {
+        switch(currentLevel){
+            
+        }
+    }
+    while( leftOverXP < 0 )
+}
+
+export default calcNewLevelAndXP;

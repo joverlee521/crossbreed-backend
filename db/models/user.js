ecb5298612ead64db7b8a6924955c4ae7d7a3cd9
kimkablitz <kim.tran549@gmail.com>
2019-01-24 17:16:49 -0800

    added sparse

--- db/models/user.js
+++ db/models/user.js
@@ -7,7 +7,7 @@ mongoose.promise = Promise
 const userSchema = new Schema({
   displayName: { type: String, unique: true, required: true },
   local: { //if using username/password
-		username: { type: String, unique: true, required: false },
+		username: { type: String, unique: true, required: false, sparse: true },
 		password: { type: String, unique: false, required: false }
   },
   google: { //if using google auth
